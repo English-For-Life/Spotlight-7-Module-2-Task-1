f0a00da79a4abf113c7a6c44744d14f0482c6ff0
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-13 17:40:47 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Choose the odd one out.",
+        options:["relieved", "scared", "shocked", "frightened"],
+        answer: 0
+    },
+    {
+        q:"Choose the odd one out.",
+        options:["cape", "pipe", "uniform", "hat"],
+        answer: 1
+    },
+    {
+        q:"Choose the odd one out.",
+        options:["ordinary", "usual", "incredible", "typical"],
+        answer: 2
+    },
+    {
+        q:"Choose the odd one out.",
+        options:["science fiction", "mystery", "comedy", "novel"],
+        answer: 3
+    },
+    {
+        q:"Choose the odd one out.",
+        options:["glance", "mystery", "drama", "myth"],
+        answer: 0
+    }
+
+
+
+]
